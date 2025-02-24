5db0505e358e558d375cd2dc0081c1352d5ca6af
mvnd06 <13355630+mvnd06@users.noreply.github.com>
2025-02-24 00:48:52 -0500

    .

--- trible-frontend/vite.config.js
+++ trible-frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "http://trible-production.up.railway.app:8080",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
